482b21a0154a10bc0de12bfb29a45a0e6fc44ea0
Shkalin Vyacheslav <Shkalin2020@yandex.ru>
2022-11-16 03:25:49 +0300

    add User Servlet and User DTO

---
+++ src/main/java/ru/kpfu/itis/shkalin/controller/UserServlet.java
@@ -0,0 +1,27 @@
+package ru.kpfu.itis.shkalin.controller;
+
+import ru.kpfu.itis.shkalin.dto.UserDto;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "userServlet", urlPatterns = "/users")
+public class UserServlet extends HttpServlet {
+
+    private static final List<UserDto> USERS = List.of(
+            new UserDto("ivan222", "sdsd@mail.ru"),
+            new UserDto("Petr1", "petro@gmail.com"),
+            new UserDto("sten2001", "stepsss2010@ya.ru")
+    );
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setAttribute("users", USERS);
+        req.getRequestDispatcher("/view/users.ftl").forward(req, resp);
+    }
+}
