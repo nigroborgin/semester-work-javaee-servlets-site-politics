994f869fd45d50a52fe757cc324cec7069676d54
Shkalin Vyacheslav <Shkalin2020@yandex.ru>
2022-11-20 04:38:19 +0300

    moved in "entity" package and add no-args constructor

--- src/main/java/ru/kpfu/itis/shkalin/entity/Comment.java
+++ src/main/java/ru/kpfu/itis/shkalin/entity/Comment.java
@@ -1,8 +1,11 @@
-package ru.kpfu.itis.shkalin.model;
+package ru.kpfu.itis.shkalin.entity;
 
-public class Comment {
+public class Comment extends AbstractEntity {
     private Integer id;
     private Integer authorId;
     private String text;
     private String date; // TODO: DB date  -->  ????
+
+    public Comment() {}
+
 }
