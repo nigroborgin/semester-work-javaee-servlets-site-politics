67f84482f43feeb69632f5349a10a2260ba37f5f
Shkalin Vyacheslav <Shkalin2020@yandex.ru>
2022-11-24 00:42:31 +0300

    add AuthenticationFilter

---
+++ src/main/java/ru/kpfu/itis/shkalin/filter/AuthenticationFilter.java
@@ -0,0 +1,42 @@
+package ru.kpfu.itis.shkalin.filter;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebFilter(filterName = "authenticationFilter", urlPatterns = {"/users", "/addArticle", "/addPost"})
+public class AuthenticationFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        String uri = request.getRequestURI();
+        HttpSession session = request.getSession(false);
+        if (session == null && !uri.contains("login")) {
+            response.sendRedirect("/politics/login");
+        } else {
+            filterChain.doFilter(servletRequest, servletResponse);
+        }
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
+}
