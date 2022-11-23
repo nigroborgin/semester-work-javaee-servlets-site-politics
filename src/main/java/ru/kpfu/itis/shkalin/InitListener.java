960a55928e80acb46873fdcc940a38ce2eee4b8b
Shkalin Vyacheslav <Shkalin2020@yandex.ru>
2022-11-24 01:00:32 +0300

    add in InitListener:  init converter, dao's, crud-services

--- src/main/java/ru/kpfu/itis/shkalin/InitListener.java
+++ src/main/java/ru/kpfu/itis/shkalin/InitListener.java
@@ -1,7 +1,10 @@
 package ru.kpfu.itis.shkalin;
 
-import ru.kpfu.itis.shkalin.service.PostListService;
+import ru.kpfu.itis.shkalin.dao.*;
+import ru.kpfu.itis.shkalin.service.converter.ConverterServiceImpl;
+import ru.kpfu.itis.shkalin.service.сrud.*;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
@@ -11,6 +14,39 @@ public class InitListener implements ServletContextListener {
 
     @Override
     public void contextInitialized(ServletContextEvent sce) {
-        sce.getServletContext().setAttribute("postListService", new PostListService());
+        ServletContext sc = sce.getServletContext();
+
+        sc.setAttribute("converter", new ConverterServiceImpl());
+
+        sc.setAttribute("bookDao", new BookDao());
+        sc.setAttribute("commentDao", new CommentDao());
+        sc.setAttribute("postDao", new PostDao());
+        sc.setAttribute("roleDao", new RoleDao());
+        sc.setAttribute("userDao", new UserDao());
+
+        sc.setAttribute("bookCrudService",
+                new BookCrudService(
+                        (BookDao) sc.getAttribute("bookDao"),
+                        (ConverterServiceImpl) sc.getAttribute("converter")));
+
+        sc.setAttribute("commentCrudService",
+                new CommentCrudService(
+                        (CommentDao) sc.getAttribute("commentDao"),
+                        (ConverterServiceImpl) sc.getAttribute("converter")));
+
+        sc.setAttribute("postCrudService",
+                new PostCrudService(
+                        (PostDao) sc.getAttribute("postDao"),
+                        (ConverterServiceImpl) sc.getAttribute("converter")));
+
+        sc.setAttribute("roleCrudService",
+                new RoleCrudService(
+                        (RoleDao) sc.getAttribute("roleDao"),
+                        (ConverterServiceImpl) sc.getAttribute("converter")));
+
+        sc.setAttribute("userCrudService",
+                new UserCrudService(
+                        (UserDao) sc.getAttribute("userDao"),
+                        (ConverterServiceImpl) sc.getAttribute("converter")));
     }
 }
